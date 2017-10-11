1cf9b0ade82ce1b949c87e504c96a60558470d63
vovmanool <v.ropalo@gmail.com>
2017-10-11 20:44:48 +0300

    less2 second commit

--- src/com/vovs/Main.java
+++ src/com/vovs/Main.java
@@ -1,16 +1,46 @@
 package com.vovs;
+import java.util.Scanner;
+
 
 public class Main {
 
+   /* public static  test1(){
+
+    }
+    public static  test2(int a, String s){
+        System.out.println("a:"+a"; s:"+s);
+    }
+    public static  test3(){
+
+    }*/
+    long a=1l;
+
+
+
     public static void main(String[] args) {
-	// write your code here
+	// write your code her
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Введи почту");
+            String result = scanner.nextLine();
+            if ("exit".equals(result)) {
+                System.out.println("Пока");
+                return;
+            }
+            int indexOfAt= result.indexOf("@");
+            //int value= Integer.valueOf("@");
+            boolean isEmail = indexOfAt>=0;
+
+            if (isEmail) {
+                System.out.println("Is email");
+            } else {
+                System.out.println("Is text");
+            }
+
+
+         //   test2(13,"AAA");
+
+        }
     }
 }
-//dfsdf
-
-/*fdfdsf*/
-/**
- *
- *
- * */
 
